055c0b30a90fe6265cf5dd66715dd67b691d9ea1
Camilo-Gomez <camilo.gomez114@gmail.com>
2025-07-15 14:10:23 -0500

    feat: log VITE_API_URL in api and authService for debugging purposes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+console.log("VITE_API_URL (PROD):", import.meta.env.VITE_API_URL);
 // Crear instancia de Axios con configuración base
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,6 +4,7 @@ export const loginService = async (credentials: {
   username: string;
   password: string;
 }) => {
+  console.log("VITE_API_URL (PROD):", import.meta.env.VITE_API_URL);
   const response = await api.post("/auth/login", credentials);
   return response.data;
 };
